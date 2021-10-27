52066f9012b40bea6044fb12d8194a794a5e733a
Ricardo Retamozo Garay <ricardo.retamozo@tecsup.edu.pe>
2021-10-27 13:16:32 -0500

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by ricardo on 10/27/21.
+//  Copyright © 2021 ricardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
